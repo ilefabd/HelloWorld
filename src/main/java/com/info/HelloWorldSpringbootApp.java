1fbc6d2243b550f8a28250d81fe448adacb2e4a7
ilef <ilef.abd@gmail.com>
2018-02-06 16:20:05 +0100

    second commit JPA

--- src/main/java/com/info/HelloWorldSpringbootApp.java
+++ src/main/java/com/info/HelloWorldSpringbootApp.java
@@ -1,5 +1,8 @@
 package com.info;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,8 +12,23 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages={"com.info.controller"})
 public class HelloWorldSpringbootApp {
 	   
+	//public static void main(String[] args) {      
+	/*  try {
+	      Class.forName("org.postgresql.Driver");
+	      System.out.println("Driver O.K.");
 
-	    public static void main(String[] args) throws Exception {
+	      String url = "jdbc:postgresql://localhost:5432/testdb";
+	      String user = "postgres";
+	      String passwd = "pg123456";
+
+	      Connection conn = DriverManager.getConnection(url, user, passwd);
+	      System.out.println("Connexion effective !");         
+	         
+	    } catch (Exception e) {
+	      e.printStackTrace();
+	    }      
+	  }*/
+	   public static void main(String[] args) throws Exception {
 	        SpringApplication.run(HelloWorldSpringbootApp.class, args);
 	    }
 }
--- src/main/java/com/info/controller/HelloWorldController.java
+++ src/main/java/com/info/controller/HelloWorldController.java
@@ -4,12 +4,61 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
-public class HelloWorldController {
+import java.util.Arrays;
 
-	    @RequestMapping("/basbouss")
-	    @ResponseBody
-	    String home() {
-	        return "Hello basbouss!";
-	    }
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.info.model.Customer;
+import com.info.repo.CustomerRepository;
+ 
+
+@RestController
+public class HelloWorldController {
+	@Autowired
+	CustomerRepository repository;
+	
+	@RequestMapping("/save")
+	public String process(){
+		// save a single Customer
+		repository.save(new Customer("ileftest", "abde"));
+		
+		// save a list of Customers
+		repository.save(Arrays.asList(new Customer("ilef", "abderrahim"), new Customer("tarek", "ghodhbani"),
+										new Customer("touma", "abderrahim"), new Customer("tarek", "mansour")));
+		
+		return "Done";
+	}
+	
+	
+	@RequestMapping("/findall")
+	public String findAll(){
+		String result = "";
+		
+		for(Customer cust : repository.findAll()){
+			result += cust.toString() + "<br>";
+		}
+		
+		return result;
+	}
+	
+	@RequestMapping("/findbyid")
+	public String findById(@RequestParam("id") long id){
+		String result = "";
+		result = repository.findOne(id).toString();
+		return result;
+	}
+	
+	@RequestMapping("/findbylastname")
+	public String fetchDataByLastName(@RequestParam("lastname") String lastName){
+		String result = "";
+		
+		for(Customer cust: repository.findByLastName(lastName)){
+			result += cust.toString() + "<br>"; 
+		}
+		
+		return result;
+	}
 }
